ac3ffbc3d5599cc1a521b57323cafb3f0fa19ebc
alexjohnives <alexanderjohnives@gmail.com>
2021-04-06 10:12:54 -0400

    Test 2

--- script.js
+++ script.js
@@ -1,21 +1,78 @@
-// document.querySelectorAll('.img-button').forEach(item => {
-//   item.addEventListener('click', event => {
-//     console.log('Hello World');  
+const imgSelected = document.querySelectorAll('.img-select');
+
+imgSelected.forEach((imgSelect) => {
+  imgSelect.addEventListener('click', ()=>{
+imgSelect.classList.toggle('active');
+  });
+});
+
+
+// ---------------
+
+function select0() {
+  var x = document.getElementById("i0div");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+ } else {
+ x.style.display = "none";
+}
+}
+   
+function select1() {
+  var x = document.getElementById("i1div");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+ } else {
+ x.style.display = "none";
+}
+}
+
+function select2() {
+var x = document.getElementById("i2div");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+x.style.display = "none";
+}
+}
+
+// -----------
+
+// var imgSelect = document.querySelectorAll('.img-select');
+// imgSelect.forEach(function(imgSelect) {
+// console.log('imgSelect: ', imgSelect);
 // })
-// });
 
-const ilse = document.querySelector('.img-button');
+// imgSelect.forEach(function)
 
-function loadEventListeners(){
-  ilse.addEventListener('click', addInfo);
-}
+// ---------
 
-function addInfo(e) {
-  const info = document.createElement('div');
-  div.className = 'container';
+//  document.querySelectorAll('.img-').forEach(item => {
+//    item.addEventListener('click', event => {
+//      console.log('Hello World');  
+//  })
+//  });
 
-}
+// $('imgButton').on('click', function() {
+//   $('imgButton').removeClass('active');
+//   $(this).addClass('active');
+// })
 
+// ---------------
+
+// const ilse = document.querySelector('.img-select');
+
+// function loadEventListeners(){
+//   ilse.addEventListener('click', addInfo);
+// }
+
+// function addInfo(e) {
+//   const info = document.createElement('div');
+//   div.className = 'container';
+
+// }
+
+// -------------------
 
 // const div = document.createElement('div');
 // div.className = 'container-fluid';
